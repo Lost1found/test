5c7492cf4912cee96a21a9dddfd080f75ee7bb67
YanWenjun0822 <1352720853@qq.com>
2018-12-02 22:17:29 +0800

    user

---
+++ user.sql
@@ -0,0 +1,7 @@
+INSERT INTO `user` VALUES ('105022222222', '22@qq.com', 'ywj980822', '舅妈');
+INSERT INTO `user` VALUES ('105032016082', '1352720853@qq.com', 'wohaoleia', '在座各位都是巨巨！');
+INSERT INTO `user` VALUES ('105032016090', '1352720853@qq.com', 'hahahahaha', '鱼丸子');
+INSERT INTO `user` VALUES ('105032016140', '1214556789@qq.com', 'zyc980822', '章鱼');
+INSERT INTO `user` VALUES ('105032016158', '111@qq.com', 'wohaoleia', 'yanwenjun');
+INSERT INTO `user` VALUES ('105032016666', '222@qq.com', '123456789', '22');
+INSERT INTO `user` VALUES ('123456789012', '1872241743@qq.com', '1234567890', '嘻嘻');
